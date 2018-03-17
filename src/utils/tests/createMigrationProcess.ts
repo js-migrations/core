ba2cc364c529798dc89b6562ee3b3838521313e2
Ryan Smith <0ryansmith1994@gmail.com>
2018-03-17 23:51:40 +0000

    ci(tslint): Fixes linting.

--- src/utils/tests/createMigrationProcess.ts
+++ src/utils/tests/createMigrationProcess.ts
@@ -3,7 +3,7 @@ import { delay } from 'bluebird';
 export default () => {
   let processed: Date | undefined; // tslint:disable-line:no-let
   const process = async () => {
-    await delay(1);
+    await Promise.resolve(delay(1));
     processed = new Date();
   };
   const getProcessed = () => {
